fa3aea0cab8f1ad4407ea3e8f63f0b5c5599fe92
DimasDarma11 <dimasdarma35@gmail.com>
2025-09-26 10:04:04 +0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -80,9 +80,6 @@ const Hero = () => {
 
           {/* Scroll indicator */}
           <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
-            <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
-              <div className="w-1 h-3 bg-white rounded-full mt-2 animate-bounce"></div>
-            </div>
           </div>
         </div>
       </div>
